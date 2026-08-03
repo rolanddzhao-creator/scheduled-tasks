ffe99a370b679d3ccc3bd34f19caa30305d04f07
rolanddzhao-creator <roland.d.zhao@gmail.com>
2026-08-03 12:29:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests, smtplib, os
-yahoo_email = os.environ.get("YAHOO_EMAIL")
-yahoo_password = os.environ.get("YAHOO_PASSWORD")
+my_email = os.environ.get("MY_EMAIL")
+my_password = os.environ.get("MY_PASSWORD")
 lay_email = os.environ.get("LAY_EMAIL")
 
 response = requests.get(url="https://www.floatrates.com/daily/usd.json")
@@ -8,7 +8,7 @@ response.raise_for_status()
 data = response.json()
 rate = data["cad"]["rate"]
 
-with smtplib.SMTP_SSL("smtp.mail.yahoo.com", port=465) as connection:
-    connection.login(user=yahoo_email, password=yahoo_password)
+with smtplib.SMTP_SSL("smtp.gmail.com", port=465) as connection:
+    connection.login(user=my_email, password=my_password)
     msg = f"Subject: Current US to CAD exchange rate\n\n1 US dollar is equal to {rate:.4f} CAD dollars"
-    connection.sendmail(from_addr=yahoo_email, to_addrs=lay_email, msg=msg)
+    connection.sendmail(from_addr=my_email, to_addrs=lay_email, msg=msg)
